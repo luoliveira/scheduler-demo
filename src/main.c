70a2b2795bda0f07cf49c50a664df9cec9a19ffc
luoliveira <oliveira.m.luana@gmail.com>
2017-10-02 23:55:03 -0300

    terminado

--- src/main.c
+++ src/main.c
@@ -3,11 +3,34 @@
 int main() {
   int a[5];
   int n;
+  int i, flag;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (i=0; i<n; i++) {
+  	flag = 1;
+    if ((i+1) % a[0] == 0) {
+      printf ("0");
+      flag = 0;
+    }
+    if ((i+1) % a[1] == 0) {
+      printf ("1");
+      flag = 0;
+    }
+    if ((i+1) % a[2] == 0) {
+      printf ("2");
+      flag = 0;
+    }
+    if ((i+1) % a[3] == 0) {
+      printf ("3");
+      flag = 0;
+    }
+    if ((i+1) % a[4] == 0) {
+      printf ("4");
+      flag = 0;
+    }
+    if (flag) printf("-");
+    printf("\n");
   }
 
   return 0;
